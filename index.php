a34c1fea9aa220423418d207a939dab79eb216ad
yaibondi <yaibondi@gmail.com>
2018-05-30 15:14:31 +0200

    normatives del concurs div

--- index.php
+++ index.php
@@ -10,7 +10,41 @@
 		<script src="js/functions.js"></script>
 	</head>
 	<body>
-		
+
+
+		<!-- inicio Normatives del concurs________________________________________________________________ -->
+		<div class="row">
+			<div class="col-sm">
+				<h2>
+					<span class="CA"></span>
+					<span class="ES">Tu si que vales</span>
+				</h2>
+				<span>Concursos de talentos del barrio el Besòs i el Maresme Viernes 15 de junio de 20h a 23h Rambla Prim con c. Ferrer Bassa</span>
+				<div>(Metro Besòs Mar-L4)</div>
+				<span>Si tienes alguna habilidad, talento, tocas algún instrumento, te gusta bailar, cantar,hacer playback, circo, teatro, magia o cualquier cosa estrambótica no te lo pienses y </span>
+				<div>¡apúntate! Concurso con premios!</div>
+
+
+
+				<span></span>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-sm">
+				lo que va de textocategorias
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-sm">
+				lo que va de inscripciones
+			</div>
+		</div>
+<!-- 		fi de normatives_______________________________________________________________________________________
+ -->
+
+
+
+
 		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
