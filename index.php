3bf682d895791aa89bca3536869b8360b790f003
rafatbd <rafatinfo4@gmail.com>
2018-05-30 15:13:35 +0200

    menu principal en header

--- index.php
+++ index.php
@@ -10,24 +10,26 @@
 		<script src="js/functions.js"></script>
 		
 		<!-- TODO: menu principal -->
-		<nav class="navbar navbar-expand-lg navbar-light bg-light">
-	  		<!-- <a class="navbar-brand" href="#">Navbar</a> -->
-	  		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-	   			 <span class="navbar-toggler-icon"></span>
-	  		</button>
-	  		<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-	    		<div class="navbar-nav">
-	      			<a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-	      			<a class="nav-item nav-link" href="#">Page 1</a>
-	      			<a class="nav-item nav-link" href="#">Page 2</a>
-	      			<a class="nav-item nav-link">
-	      				<h1>Tu Si Que Vales!</h1>
-	      			</a>
-	      			<a ></a>
-	      			<a ></a>
-	    		</div>
-	  		</div>
-		</nav>
+		<header>
+			<nav class="navbar navbar-expand-lg navbar-light bg-light">
+		  		<!-- <a class="navbar-brand" href="#">Navbar</a> -->
+		  		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+		   			 <span class="navbar-toggler-icon"></span>
+		  		</button>
+		  		<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+		    		<div class="navbar-nav">
+		      			<a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+		      			<a class="nav-item nav-link" href="#">Page 1</a>
+		      			<a class="nav-item nav-link" href="#">Page 2</a>
+		      			<a class="nav-item nav-link">
+		      				<h1>Tu Si Que Vales!</h1>
+		      			</a>
+		      			<a class="nav-item nav-link" href="#">ESP</a>
+		      			<a class="nav-item nav-link" href="#">CAT</a>
+		    		</div>
+		  		</div>
+			</nav>
+		</header>
 	</head>
 	<body>
 		
