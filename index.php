fd03baf7806a94e9291115de16e38a61a4ed3e82
rafatbd <rafatinfo4@gmail.com>
2018-05-30 14:54:43 +0200

    Menu principal(nav)

--- index.php
+++ index.php
@@ -8,6 +8,26 @@
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 		<link rel="stylesheet" type="text/css" href="css/style.css">
 		<script src="js/functions.js"></script>
+		
+		<!-- TODO: menu principal -->
+		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+	  		<!-- <a class="navbar-brand" href="#">Navbar</a> -->
+	  		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+	   			 <span class="navbar-toggler-icon"></span>
+	  		</button>
+	  		<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+	    		<div class="navbar-nav">
+	      			<a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+	      			<a class="nav-item nav-link" href="#">Page 1</a>
+	      			<a class="nav-item nav-link" href="#">Page 2</a>
+	      			<a class="nav-item nav-link">
+	      				<h1>Tu Si Que Vales!</h1>
+	      			</a>
+	      			<a ></a>
+	      			<a ></a>
+	    		</div>
+	  		</div>
+		</nav>
 	</head>
 	<body>
 		
