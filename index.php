a5014591c30842abb5c36ceb843ea6da5749dc1a
rafatbd <rafatinfo4@gmail.com>
2018-05-31 13:22:42 +0200

    Actualizando header

--- index.php
+++ index.php
@@ -14,8 +14,8 @@
 	<body>
 		<!-- TODO: menu principal -->
 		<header>
-			<div class="container d-flex d-block fixed-top bg-white mb-5">
-			    <div class="mr-auto p-2">
+			<!-- <div class="container d-flex d-block fixed-top bg-white mb-5"> -->
+			    <!-- <div class="mr-auto p-2">
 			    	<a class="textBlack" href="#">Home <span class="sr-only">(current)</span></a>	
 			    </div>
 			    <div class="mr-auto p-2">
@@ -26,7 +26,34 @@
 			    </div>
 			    <div class="mr-auto p-2">
 					<a class="textBlack">
-			      		<h1>Tu Si Que Vales!</h1>
+			      		<h1>
+			      			Tu Si Que Vales!
+			      		</h1>
+			      	</a>
+			    </div>
+				<div class="ml-auto p-2">
+					<a class="textBlack" href="#">ESP</a>
+				</div>
+	  			<div class="ml-auto p-2">
+	  				<a class="textBlack" href="#">CAT</a>
+	  			</div> -->
+			<!-- </div> -->
+			<div class="col-sm mt-5 position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
+				<div class="container d-flex d-block fixed-top bg-white mb-5">
+					 <div class="mr-auto p-2">
+			    	<a class="textBlack" href="#">Home <span class="sr-only">(current)</span></a>	
+			    </div>
+			    <div class="mr-auto p-2">
+			    	<a class="textBlack" href="#">Page 1</a>
+			    </div>
+			    <div class="mr-auto p-2">
+			    	<a class="textBlack" href="#">Page 2</a>
+			    </div>
+			    <div class="mr-auto p-2">
+					<a class="textBlack">
+			      		<h1>
+			      			Tu Si Que Vales!
+			      		</h1>
 			      	</a>
 			    </div>
 				<div class="ml-auto p-2">
@@ -35,6 +62,8 @@
 	  			<div class="ml-auto p-2">
 	  				<a class="textBlack" href="#">CAT</a>
 	  			</div>
+					
+				</div>
 			</div>	
 		</header>
 
