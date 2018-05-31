30972265c7d23a2c87e6807a44a7e0edde3e708a
oscarmoreno9 <36258571+oscarmoreno9@users.noreply.github.com>
2018-05-31 14:04:57 +0200

    coger foto escalada

--- index.php
+++ index.php
@@ -139,9 +139,8 @@
 		</div>
 		<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 		<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-		<!-- inicio footer mx-5-->
-		<footer class=" borderFooter">
-
+		<!-- inicio footer-->
+		<footer class="borderFooter">
 			<img class="col-3 col-md-3 col-lg-1 img-fluid" src="img/logo PDC.jpg">
 			<img class="col-3 col-md-3 col-lg-1 img-fluid" src="img/logocalco2_sense_marc.jpg">
 			<img class="col-3 col-md-3 col-lg-1 img-fluid" src="img/cazcarra.jpg">
@@ -153,7 +152,7 @@
 			<img class="col-3 col-md-3 col-lg-1 img-fluid" src="img/Logotipo_SOC_color_png.png">
 			<img class="col-3 col-md-3 col-lg-1 img-fluid" src="img/logoGeneralitatColor_png.png">
 			<img class="col-3 col-md-3 col-lg-1 img-fluid" src="img/MEYSS_color.jpg">
-			<img class="col-3 col-md-3 col-lg-1 img-fluid" src="img/logocalco2_sense_marc.jpg">
+			<img class="col-3 col-md-3 col-lg-1  src="img/logocalco2_sense_marc.jpg">
 		</footer>
 		<!-- fin footer -->
 
