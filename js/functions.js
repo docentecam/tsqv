0adf4fede38a386a718f0a86a65480c772d8667f
yaibondi <yaibondi@gmail.com>
2018-05-31 10:02:19 +0200

    agregar funcion que cambia el idioma de catalan a español

--- js/functions.js
+++ js/functions.js
@@ -8,3 +8,27 @@ function muestraDivVideo(valor){
 		miDiv.className="oculto";
 	}
 }
+
+function canviaIdioma(idioma) {
+	setCookie('selIdi',idioma,365);
+	mostrarIdiomes();
+}
+function mostrarIdiomes()
+{
+	var ocultaCA;
+	var ocultaES;
+	if(getCookie('selIdi')=="ES") {
+		ocultaCA="none";
+		ocultaES="inline";
+	}
+	else{
+		ocultaCA="inline";
+		ocultaES="none";
+	}
+	objetosCA=document.getElementsByClassName("CA");
+	objetosES=document.getElementsByClassName("ES");
+	for(i=0;i<objetosCA.length;i++){
+		objetosCA[i].style.display=ocultaCA;
+		objetosES[i].style.display=ocultaES;
+	}
+}
