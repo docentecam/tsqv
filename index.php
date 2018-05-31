57eb5cea3dd248c5c86af996c98e527ecc73cc43
yaibondi <yaibondi@gmail.com>
2018-05-31 12:24:02 +0200

    cambio de color iconos y recuadros para darle mas estilo a la pagina

--- index.php
+++ index.php
@@ -40,39 +40,7 @@
 
 
 		<!-- inicio Normatives del concurs________________________________________________________________ -->
-		<!-- <div class="row">
-			<div class="col-sm text-center mt-5" >
-				<h1>
-					<span class="CA">Tu si que vales</span>
-					<span class="ES" >Tu si que vales</span>
-				</h1>
-				<span class="ES">Concursos de talentos del barrio el Besòs i el Maresme</span><span class="CA">Concurs de talents al barri el Besòs i el Maresme</span><br>
-				<span class="ES" style="display: none">Viernes 15 de junio de 20h a 23h Rambla Prim con c. Ferrer Bassa</span><span class="CA">Divendres 15 de juny de 20h a 23h a Rambla Prim amb c. Ferrer Bassa </span><br>
-				<span class="ES" style="display: none">(Metro Besòs Mar-L4)</span> <span class="CA">(Metro Besòs Mar-L4)</span><br>
-				<span class="ES" style="display: none">Si tienes alguna habilidad, talento, tocas algún instrumento, te gusta bailar, cantar,hacer playback, circo, teatro, magia o cualquier cosa estrambótica no te lo pienses y </span><span class="CA">Si tens alguna habilitat, talent, toques algun instrument, t’agrada ballar, cantar, fer
-		        playback, circ, teatre, màgia o qualsevol cosa estrambòtica no t’ho pensis i apunta’t! </span><br>
-				<span class="ES">¡apúntate! Concurso con premios!</span><span class="CA">És un concurs amb premis!</span><br>
-				<span class="ES">Descarga el diptico en los siguientes idiomas en  català, castellà, àrab, xinés i wolof<br><a href="documents/TSQVDptc.pdf" target="_blank">Pulsa aqui</a></span><br>
-				<span class="CA">Descarrega el diptico en els següents idiomes en català, castellà, àrab, xinés i wolof<br><a href="documents/TSQVDptc.pdf" target="_blank">prem aqui</a></span><br>	
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-sm text-center">
-					<span class="ES">3 CATEGORÍAS:</span><span class="CA">3 CATEGORIES</span><br>
-					<span class="ES"><i class="fas fa-child"></i>Infantil (hasta 12 años)</span><span class="CA"><i class="fas fa-child"></i>Infantil (fins a 12 anys)</span><br>
-					<span class="ES"><i class="fas fa-graduation-cap"></i>Juvenil (hasta 25 años)</span><span class="CA"><i class="fas fa-graduation-cap"></i>Juvenil (fins a 25 anys)</span><br>
-					<span class="ES"><i class="fas fa-male"></i>Adulta (mayores 25 años)</span><span class="CA"><i class="fas fa-male"></i>Adults (majors 25 anys)</span>
-			</div>
-		</div><br>
-		<div class="row">
-			<div class="col-sm text-center" >
-				<span class="ES text-center">INSCRIPCIONES</span><br>
-				<span class="ES">Hasta el 10 de junio, presencialmente en el Centre Cívic Besòs(rambla Prim 87-89,
-				 Metro L4 Besòs Mar)	o enviando un mail a tusiquevales.bm@gmail.com
-				Más info al 93 256 49 01 / 617 252 908</span>
-			</div>
-		</div>
-	</div> -->
+		
 		
 				<div class="row">
 					<div class="col-sm mt-5 position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
@@ -86,20 +54,21 @@
 						<span class="ES">Si tienes alguna habilidad, talento, tocas algún instrumento, te gusta bailar, cantar,hacer playback, circo, teatro, magia o cualquier cosa estrambótica no te lo pienses y </span><span class="CA" style="display: none">Si tens alguna habilitat, talent, toques algun instrument, t’agrada ballar, cantar, fer
 				        playback, circ, teatre, màgia o qualsevol cosa estrambòtica no t’ho pensis i apunta’t! </span><br>
 						<span class="ES">¡apúntate! Concurso con premios!</span><span class="CA" style="display: none" >És un concurs amb premis!</span><br>
-						<span class="ES">Descarga el diptico en los siguientes idiomas en  català, castellà, àrab, xinés i wolof<br><a class="btn btn-outline-secondary" href="documents/TSQVDptc.pdf" target="_blank">Pulsa aqui</a></span><br>
-						<span class="CA" style="display: none">Descarrega el diptico en els següents idiomes en català, castellà, àrab, xinés i wolof<br><a class="btn btn-outline-secondary" href="documents/TSQVDptc.pdf" target="_blank">prem aqui</a></span><br>	
+						<span class="ES">Descarga el diptico en los siguientes idiomas en  català, castellà, àrab, xinés i wolof<br><a class="btn btn-outline-info" href="documents/TSQVDptc.pdf" target="_blank">Pulsa aqui</a></span><br>
+						<span class="CA" style="display: none">Descarrega el diptico en els següents idiomes en català, castellà, àrab, xinés i wolof<br><a class="btn btn-outline-info" href="documents/TSQVDptc.pdf" target="_blank">prem aqui</a></span><br>	
 					</div>
 				</div>
 				<div class="row">
-					<div class="col-sm text-center">
+					<div class="col-sm text-center position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
 							<span class="ES bg-info text-white">3 CATEGORÍAS:</span><span class="CA bg-info text-white" style="display: none">3 CATEGORIES</span><br>
 							<span class="ES"><i class="fas fa-child"></i>Infantil (hasta 12 años)</span><span class="CA" style="display: none"><i class="fas fa-child"></i>Infantil (fins a 12 anys)</span><br>
 							<span class="ES"><i class="fas fa-graduation-cap"></i>Juvenil (hasta 25 años)</span><span class="CA" style="display: none"><i class="fas fa-graduation-cap"></i>Juvenil (fins a 25 anys)</span><br>
 							<span class="ES"><i class="fas fa-male"></i>Adulta (mayores 25 años)</span><span class="CA" style="display: none"><i class="fas fa-male"></i>Adults (majors 25 anys)</span>
+	
 					</div>
 				</div><br>
 				<div class="row">
-					<div class="col-sm text-center" >
+					<div class="col-sm text-center position-relative overflow-hidden p-3 p-md-5 m-md-2 text-center bg-light" >
 						<span class="ES text-center bg-info text-white" >INSCRIPCIONES</span><span class="CA bg-info text-white" style="display: none">INSCRIPCIONS </span><br>
 						<span class="ES" >Hasta el 10 de junio, presencialmente en el Centre Cívic Besòs</span><span class="CA" style="display: none">Fins al 10 de juny presencialment al Centre Cívic Besòs </span><br>
 						<span class="ES" >(rambla Prim 87-89, Metro L4 Besòs Mar)</span><span class="CA" style="display: none">(rambla Prim 87-89, Metro L4 Besòs Mar)</span><br>
