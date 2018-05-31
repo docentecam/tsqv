e617cd17a1234ad6b5f2d550fdf92c768beaee73
rtalyon <rta.lyon@gmail.com>
2018-05-31 08:39:42 +0200

    css, js y php

--- index.php
+++ index.php
@@ -69,22 +69,21 @@
 
 
 		<!--*************************************COMIENZO VIDEOS*******************-->
+		<div class="row">
+			<div id="idVideos">
 
-		<div id="idVideos">
-
-		<!-- inicio form -->
-			<div>
-				<button onclick="muestraDivVideo()"><i class="far fa-plus"></i></button>
-				<form id="formVideo">
-					<input type="text" name="">
-				</form>
+			<!-- inicio form -->
+				<div class="row">
+					<button onclick="muestraDivVideo('vid')" class=" far fa-plus"></button>
+					<form id="formVideo" class="col-12">
+						<input type="text" name="">
+					</form>
+				</div>
+			<!-- fin form -->	
+			<!--****-COMIENZO VIDEOS***-->
+			<!--***-FINAL VIDEOS**-->
 			</div>
-		<!-- fin form -->	
-		<!--****-COMIENZO VIDEOS***-->
-		<!--***-FINAL VIDEOS**-->
 		</div>
-		
-
 
 		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
