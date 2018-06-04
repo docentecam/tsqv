8fce93e5b673e53d96b0cfafd70e0bdc0dfdb2e4
oscarmoreno9 <36258571+oscarmoreno9@users.noreply.github.com>
2018-06-04 08:04:14 +0200

    nombres logos cambiados

--- index.php
+++ index.php
@@ -163,17 +163,17 @@
 		</div>
 		
 		<footer class="mx-5 borderFooter">
-			<img class="col-lg-1 col-sm-4 col-md-2 img-fluid" src="img/logo PDC.jpg">
-			<img class="col-lg-1 col-sm-4 col-md-2 img-fluid" src="img/logocalco2_sense_marc.jpg">
+			<img class="col-lg-1 col-sm-4 col-md-2 img-fluid" src="img/plaComunitari.jpg">
+			<img class="col-lg-1 col-sm-4 col-md-2 img-fluid" src="img/centreCivic.jpg">
 			<img class="col-lg-1 col-sm-4 col-md-2 img-fluid" src="img/LogoAvMaresme.jpg">
 			<img class="col-lg-1 col-sm-4 col-md-2 img-fluid" src="img/cazcarra.jpg">
-			<img class="col-lg-1 col-sm-4 col-md-2 img-fluid" src="img/BFD_DEFINITIVO.jpg">
-			<img class="col-lg-1 col-sm-4 col-md-2 img-fluid" src="img/Logo_DTASF-Direccio_General_Accio_Civica_Comunitaria (1).jpg">
-			<img class="col-lg-1 col-sm-4 col-md-2 img-fluid" src="img/Aj+basa_2014_rodona_color-fonsblanc.png">
-			<img class="col-lg-1 col-sm-4 col-md-2 img-fluid" src="img/Logo-CasesOficis-2018- color.png">
-			<img class="col-lg-1 col-sm-4 col-md-2 img-fluid" src="img/Logotipo_SOC_color_png.png">
-			<img class="col-lg-1 col-sm-4 col-md-2 img-fluid" src="img/LogoGeneralitatcolor_png.png">
-			<img class="col-lg-1 col-sm-4 col-md-2 img-fluid" src="img/MEYSS_color.jpg">	
+			<img class="col-lg-1 col-sm-4 col-md-2 img-fluid" src="img/barcelonaForumDistrict.jpg">
+			<img class="col-lg-1 col-sm-4 col-md-2 img-fluid" src="img/Direccio_General_Accio_Civica.jpg">
+			<img class="col-lg-1 col-sm-4 col-md-2 img-fluid" src="img/bcnActiva.png">
+			<img class="col-lg-1 col-sm-4 col-md-2 img-fluid" src="img/casesOficis.png">
+			<img class="col-lg-1 col-sm-4 col-md-2 img-fluid" src="img/Logotipo_SOC.png">
+			<img class="col-lg-1 col-sm-4 col-md-2 img-fluid" src="img/logoGeneralitat.png">
+			<img class="col-lg-1 col-sm-4 col-md-2 img-fluid" src="img/MEYSS.jpg">	
 		</footer>		
 	</body>
 </html>
