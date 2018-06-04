9588a3aa1dd5122f9a4337040107cd1f521e7d21
oscarmoreno9 <36258571+oscarmoreno9@users.noreply.github.com>
2018-06-04 10:22:28 +0200

    logo incorporada
    me habeis borrado un logo mala gente, ¡¡¡lo vuelvo a poner!!!

--- index.php
+++ index.php
@@ -173,12 +173,13 @@
 			</div>
 		</div>
 		
-		<footer class="mx-5 borderFooter">
+		<footer class="offset-1 borderFooter">
 			<img class="col-lg-1 col-sm-4 col-md-2 img-fluid" src="img/plaComunitari.jpg">
 			<img class="col-lg-1 col-sm-4 col-md-2 img-fluid" src="img/centreCivic.jpg">
 			<img class="col-lg-1 col-sm-4 col-md-2 img-fluid" src="img/LogoAvMaresme.jpg">
 			<img class="col-lg-1 col-sm-4 col-md-2 img-fluid" src="img/cazcarra.jpg">
 			<img class="col-lg-1 col-sm-4 col-md-2 img-fluid" src="img/barcelonaForumDistrict.jpg">
+			<img class="col-lg-1 col-sm-4 col-md-2 img-fluid" src="img/hotelSB.jpg">	
 			<img class="col-lg-1 col-sm-4 col-md-2 img-fluid" src="img/Direccio_General_Accio_Civica.jpg">
 			<img class="col-lg-1 col-sm-4 col-md-2 img-fluid" src="img/bcnActiva.png">
 			<img class="col-lg-1 col-sm-4 col-md-2 img-fluid" src="img/casesOficis.png">
