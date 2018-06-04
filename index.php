106c8d1b24eb295b5633ea28d2c25b85fa02dd44
oscarmoreno9 <36258571+oscarmoreno9@users.noreply.github.com>
2018-06-04 12:03:32 +0200

    estructura codigo mejor hecha

--- index.php
+++ index.php
@@ -151,16 +151,16 @@
 		</div>
 		
 		<footer class="borderFooter">
-			<img class="col-lg-1 col-2 col-md-2 img-fluid" src="img/plaComunitari.jpg">
-			<img class="col-lg-1 col-2 col-md-2 img-fluid" src="img/centreCivic.jpg">
-			<img class="col-lg-1 col-2 col-md-2 img-fluid" src="img/LogoAvMaresme.jpg">
-			<img class="col-lg-1 col-2 col-md-2 img-fluid" src="img/3logos_photoshop.png">
-			<img class="col-lg-1 col-2 col-md-2 img-fluid" src="img/Direccio_General_Accio_Civica.jpg">
-			<img class="col-lg-1 col-2 col-md-2 img-fluid" src="img/bcnActiva.png">
-			<img class="col-lg-1 col-2 col-md-2 img-fluid" src="img/casesOficis.png">
-			<img class="col-lg-1 col-2 col-md-2 img-fluid" src="img/Logotipo_SOC.png">
-			<img class="col-lg-1 col-2 col-md-2 img-fluid" src="img/logoGeneralitat.png">
-			<img class="col-lg-1 col-2 col-md-2 img-fluid" src="img/MEYSS.jpg">	
+			<img class="col-2 col-lg-1 col-md-2 img-fluid" src="img/plaComunitari.jpg">
+			<img class="col-2 col-lg-1 col-md-2 img-fluid" src="img/centreCivic.jpg">
+			<img class="col-2 col-lg-1 col-md-2 img-fluid" src="img/LogoAvMaresme.jpg">
+			<img class="col-2 col-lg-1 col-md-2 img-fluid" src="img/3logos_photoshop.png">
+			<img class="col-2 col-lg-1 col-md-2 img-fluid" src="img/Direccio_General_Accio_Civica.jpg">
+			<img class="col-2 col-lg-1 col-md-2 img-fluid" src="img/bcnActiva.png">
+			<img class="col-2 col-lg-1 col-md-2 img-fluid" src="img/casesOficis.png">
+			<img class="col-2 col-lg-1 col-md-2 img-fluid" src="img/Logotipo_SOC.png">
+			<img class="col-2 col-lg-1 col-md-2 img-fluid" src="img/logoGeneralitat.png">
+			<img class="col-2 col-lg-1 col-md-2 img-fluid" src="img/MEYSS.jpg">	
 		</footer>		
 	</body>
 </html>
